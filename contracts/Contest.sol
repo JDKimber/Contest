98607cd5e2739ba2e8ce080b48b2d06cad88c9e7
JDKimber <jdkimber@yahoo.com>
2018-10-13 22:10:42 -0400

    adds multiple contestants

--- contracts/Contest.sol
+++ contracts/Contest.sol
@@ -1,9 +1,26 @@
 pragma solidity 0.4.24;
 
 contract Contest {
-  string public contestant;
+  //creating structure to model the contestant
+  struct Contestant {
+    uint id;
+    string name;
+    uint voteCount;
+  }
 
-  function Contest() public {
-    contestant = "Tom";
+  //use mapping to get or fetch the contestant details
+  mapping(uint => Contestant) public contestants;
+
+  //add a public state variable to keep track of contestant count
+  uint public contestantsCount;
+
+  constructor () public {
+    addContestant("Tom");
+    addContestant("Jerry");
+  }
+
+  function addContestant(string _name) private{
+    contestantsCount ++;
+    contestants[contestantsCount] = Contestant(contestantsCount, _name, 0);
   }
 }
